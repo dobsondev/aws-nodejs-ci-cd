7112700d48dedee3ef88630021de9c4f3bfe41a7
alex <alex@yellowpencil.com>
2023-07-24 15:34:20 -0600

    Added new endpoint to try out.

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Node.js CI/CD Demo Application')
 })
 
+app.get('/meow', (req, res) => {
+  res.send('I am a cat')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
